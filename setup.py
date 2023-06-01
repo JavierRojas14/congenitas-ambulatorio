ab5a4e2a7d965f92c49d7982bd6881bb9e1f9c8d
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-06-01 12:27:38 -0400

    Agrega estructura inicial del repositorio

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is the repository to analyze and process the data for "Patologias Congenitas" of the "Instituto Nacional del Torax"',
+    author='Javier Rojas',
+    license='MIT',
+)
